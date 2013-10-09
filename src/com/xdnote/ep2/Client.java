5d6b0aa42dd24ea852ef49797706c36f8f8a4f4f
ada87 <ada87@163.com>
2013-10-09 14:56:02 +0800

    Signed-off-by: ada87 <ada87@163.com>

---
+++ src/com/xdnote/ep2/Client.java
@@ -0,0 +1,16 @@
+package com.xdnote.ep2;
+/**
+ * @author xdnote.com
+ * 题目：
+ * 给定一个十进制的下整数N，写下从1开始，到N的所有整数，然后数一下其中出现所有“1”的个数
+ * 例如 N=2 写下1，2，出现了1个1
+ *        N=12 写下 123456789101112 出现了5个1
+ *  问题是
+ *  1.写一个函数 F(n)，返回1到N之间出现的1的个数，比如 F(12)=5
+ *  2.在32位速数范围内，满足条件 "F(n)=n"的最大的N是多少
+ * */
+public class Client {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
