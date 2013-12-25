5f5c1fe173b58da3217af3637f242e271ebf602a
ada87 <ada87@163.com>
2013-12-25 20:31:56 +0800

    Signed-off-by: ada87 <ada87@163.com>

--- javascripts/page-effect.js
+++ javascripts/page-effect.js
@@ -3,14 +3,18 @@
 *使用方法一：直接嵌入
 * <script type="text/javascript" src="http://www.xdnote.com/javascripts/page-effect.js" ></script>
 *使用方法二：将以下脚本拖到书签，点
-javascript: void((function() {if(pageEffect)return;var element = document.createElement('script');element.charset = 'utf-8';element.setAttribute('src', 'http://www.xdnote.com/javascripts/page-effect.js');document.body.appendChild(element);})())
+javascript: void((function() {if(typeof pageEffect!='undefined'){pageEffect.change();return;};var element = document.createElement('script');element.charset = 'utf-8';element.setAttribute('src', 'http://www.xdnote.com/javascripts/page-effect.js');document.body.appendChild(element);})())
 */
 pageEffect = !function(document,window,undefined){
 	'use strict';
 	function Effects(){
 		alert(0);
 	}
+	function change(){
+
+	}
 	return {
-		'effect':Effects;
+		effect:Effects,
+		change:change
 	}
 }(document,window);
