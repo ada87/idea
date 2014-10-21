a1e0fcca75b57e9351190975a3c7fbc89057a5db
xdnote <ada87@163.com>
2014-10-21 16:59:28 +0800

    Signed-off-by: xdnote <ada87@163.com>

--- javascripts/ImgWall.js
+++ javascripts/ImgWall.js
@@ -5,71 +5,76 @@
 		fn(true);
 	}
 })(function(gobal){
-	//单例模式
-	var _self = null;
+	//单例imgWall 指针， 触屏对象，以及浏览器版本
+	var _self = null,TOUTH=null,bsv=(function () {
+	  		var tmpStyle = document.createElement("div").style;
+	  		var lists = ['t','webkitT', 'MozT', 'msT', 'OT'];
+	  		var v = '';
+		  	for (var i = 0, iLen = lists.length; i < iLen; i++) {
+		  		v = lists[i] + 'ransform';
+		  		if (v in tmpStyle) {
+		  			return lists[i];
+		  		}
+		  	}
+		  	return '';
+	  	}());
 	//配置及数据存放
 	var CONFIG = {
 		CONTAINER_ID : '',			//	容器ID
+		MIN_CROSS:50,				//	图片最小移动距离
 		SCALE:true,					//	开启放大缩小
-		ROTATE:false,				//	开启旋转
+		ROTATE:true,				//	开启旋转
+		SCALE_MAX:3,				//	最大可放大的级别
 		AUTO_OPEN : true,			//	自动启动,如果设为false，只会new一个imgwall,手动调用一下imgwall.open方法即可打开
-		AUTO_PLAY : false,			//	是否自动播放
-		AUTO_PLAY_LIMIT : 0,		//	自动播放时间设置
+		TRAN_TIME:1,				//	切换过渡时间（秒）
 		DATAS:[]					//	图片数组
 	};
 	//计算样式的数据
 	var STYLE = {
 		WIN_WIDTH:0,				//	浏览器宽度
 		WIN_HEIGHT:0,				//	浏览器高度
-		TRANSFORM:(function () {	//	浏览器的变化属性名
-	  		var tmpStyle = document.createElement("div").style;
-	  		var lists = ['t', 'webkitT', 'MozT', 'msT', 'OT'];
-	  		var transform = '';
-		  	for (var i = 0, iLen = lists.length; i < iLen; i++) {
-		  		transform = lists[i] + 'ransform';
-		  		if (transform in tmpStyle) {
-		  			return transform;
-		  		}
-		  	}
-		  	return transform;
-	  	}())
+		TRANSFORM:bsv==''?'transform':bsv+'ransform',		//	浏览器特效的样式
+		TRANSITION:bsv==''?'transition':bsv+'ransition'		//	浏览器过渡的样式
 	};
 	//页面元素
 	var ELEMENTS = {
 		ROOT:null,					//	图片墙-容器根节点
 		HEAD:null,					//	头部工具条
 		WALL:null,					//	图片墙-列表根节点
+		IMG:null,					//	图片墙上当前的图片
 		FOOT:null					//	尾部工具条
 	};
 	//事件
 	var EVENTS = {
 		OPEN_EVENT:null,			//	打开时的事件
 		CLOSE_EVENT:null,			//	关闭时的事件
-		SWITCH_EVENT:null,			//	切换图片时的事件
-		TOUTH_EVENT:null			//	点击图片时的事件
+		SWITCH_EVENT:null			//	切换图片时的事件
 	};
 	//状态存储
 	var STATE = {
 		BAR_SHOW:true,				//	状态栏是否正在显示
-		SCALE_LAST:1,				//	最后一次的SCALE的大小
-		LOCATE_LAST:1,				//	最后一次旋转的大小
+		ZOOM_LAST:1,				//	最后一次的SCALE的大小
+		ROTATE_LAST:0,				//	最后一次旋转的大小
+		X_LAST:0,					//	最后一次平移的X轴
+		Y_LAST:0,					//	最后一次平移的Y轴
+		X_TMP:0,					//	X轴偏移	
+		Y_TMP:0,					//	Y轴偏移
 		CURRENT_INDEX:0				//	当前浏览图片的位置
-		
 	};
-	//触屏对象
-	var TOUTH = null;
+	
 	function _initDefault(config){
 		CONFIG.CONTAINER_ID = typeof config.id!='undefined'?config.id:'';
 		CONFIG.AUTO_OPEN = typeof config.auto_open!='undefined' ? config.auto_open:CONFIG.AUTO_OPEN;
 		
-		
 		CONFIG.SCALE = typeof config.scale!='undefined' ? config.scale:CONFIG.SCALE;
 		CONFIG.ROTATE = typeof config.rotate!='undefined' ? config.rotate:CONFIG.ROTATE;
 		
-		CONFIG.AUTO_PLAY = typeof config.auto_play!='undefined' ? config.auto_play:CONFIG.AUTO_PLAY;
-		CONFIG.AUTO_PLAY_LIMIT = typeof config.auto_play_limit!='undefined' ? config.auto_play_limit:CONFIG.AUTO_PLAY_LIMIT;
 		CONFIG.DATAS = typeof config.data!='undefined' ? config.data:CONFIG.DATAS;
 		
+		_self.onOpen(config.evt_Open);
+		_self.onClose(config.evt_Close);
+		_self.onSwitch(config.evt_Switch);
+//		document.documentElement.clientHeight
 		STYLE.WIN_HEIGHT=document.body.offsetHeight;
 		STYLE.WIN_WIDTH = document.body.offsetWidth;
 		
@@ -83,7 +88,7 @@
 			ELEMENTS.ROOT=document.createElement('article');
 			document.body.appendChild(ELEMENTS.ROOT);
 		}
-		var headerHTML = '<section class="iw_header" id="'+hid+'">返回</section>';
+		var headerHTML = '<section class="iw_header" id="'+hid+'"><span class="iw_close" id="iw_close">&lt; 返回</span></section>';
 		var wallHTML = '<section class="iw_wall"><div id="'+wid+'" class="iw_wall_ct"></div></section>';
 		var footHTML = '<section class="iw_footer" id="'+fid+'"></section>';
 		ELEMENTS.ROOT.innerHTML = headerHTML+wallHTML+footHTML;
@@ -96,16 +101,20 @@
 		ELEMENTS.ROOT.style.width = STYLE.WIN_WIDTH+'px';
 		ELEMENTS.WALL.style.width = STYLE.WIN_WIDTH*CONFIG.DATAS.length+'px';
 	    
+	    document.getElementById('iw_close').addEventListener('click',function(){
+	    	_self.close();
+	    });
+	    
 		var wallImgs = '';
 		for(var i=0,j=CONFIG.DATAS.length;i<j;i++){
 			var img = CONFIG.DATAS[i];
-			wallImgs += '<div class="iw_img" style="background-image:url(' + img.imgUrl +');width:'+STYLE.WIN_WIDTH+'px;"></div>';
+			wallImgs += '<div class="iw_img" style="width:'+STYLE.WIN_WIDTH+'px;"><div id="iw_img_' + i + '" style="background-image:url(' + img.imgUrl +');"></div> </div>';
 		}
 		ELEMENTS.WALL.innerHTML = wallImgs;
+		ELEMENTS.IMG = document.getElementById('iw_img_0');
 	}
 	function _initEvent(){
 		TOUTH = Hammer(ELEMENTS.WALL);
-		TOUTH.LOCK = false;
 		//点击屏幕
 		var tapHandder = function(evt){
 			if(STATE.BAR_SHOW){
@@ -119,59 +128,59 @@
 			}
 		}
 		var panHandder = function(evt){
-//			console.log(evt);
-			_self.slightMove(evt.deltaX);
+			if(STATE.ZOOM_LAST > 1){
+				STATE.X_TMP = STATE.X_LAST + evt.deltaX;
+				STATE.Y_TMP = STATE.Y_LAST + evt.deltaY;
+				_self.transition();
+			}else{
+				_self.slightMove( 0 - evt.deltaX);
+			}
 		}
 		//移动
 		var panendHandder = function(evt){
-	    	if(evt.deltaX > 0){
-	    		var index = STATE.CURRENT_INDEX-1;
-	    		index = index<0?0:index;
-	    	}else{
-	    		var index = STATE.CURRENT_INDEX+1;
-	    		index = index>CONFIG.DATAS.length?CONFIG.DATAS.length:index;
-	    	}
-	    	_self.toIndex(index);
+			if(STATE.ZOOM_LAST > 1){
+				STATE.X_LAST = STATE.X_TMP;
+				STATE.Y_LAST = STATE.Y_TMP;
+			}else{
+				var index = STATE.CURRENT_INDEX;
+				if(Math.abs(evt.deltaX)<CONFIG.MIN_CROSS){
+					
+				}else if(evt.deltaX > 0){
+		    		index = STATE.CURRENT_INDEX-1;
+		    	}else{
+		    		index = STATE.CURRENT_INDEX+1;
+		    	}
+		    	_self.toIndex(index);				
+			}
 		}
 	    TOUTH.on('tap', tapHandder);
 	    TOUTH.on('pan', panHandder);
 	    TOUTH.on('panend', panendHandder);
 		
-		
 		//放大缩小
 		var pinchHandder = function(evt){
-		}
-		//旋转
-		var rotateHandder = function(evt){
-			alert(evt.rotation);
-//			alert('rotateend');
+			STATE.ZOOM_LAST*=Math.sqrt(Math.sqrt(Math.sqrt(evt.scale)));
+			STATE.ZOOM_LAST=STATE.ZOOM_LAST>1?STATE.ZOOM_LAST:1;
+			STATE.ZOOM_LAST = STATE.ZOOM_LAST>CONFIG.SCALE_MAX?CONFIG.SCALE_MAX:STATE.ZOOM_LAST;
+			STATE.ROTATE_LAST=CONFIG.ROTATE?evt.rotation:0;
+			_self.transition();
 		}
 		
-		var defaultAction  = function(evt){
-			alert(evt.type);
+		if(CONFIG.SCALE){
+			var pinch = new Hammer.Pinch();
+			if(CONFIG.ROTATE){
+				var rotate = new Hammer.Rotate();
+				pinch.recognizeWith(rotate);
+				TOUTH.add([pinch,rotate]);
+			}else{			
+				TOUTH.add([pinch]);
+			}
+		    TOUTH.on('pinchin', pinchHandder);
+		    TOUTH.on('pinchout', pinchHandder);
 		}
-
-//		var pinch = new Hammer.Pinch();
-//		var rotate = new Hammer.Rotate();
-//		pinch.recognizeWith(rotate);
-//		TOUTH.add([pinch, rotate]);
-		
-//	    TOUTH.on('pinchstart', defaultAction);
-//	    TOUTH.on('pinchmove', defaultAction);
-//	    TOUTH.on('pinchend', defaultAction);
-//	    TOUTH.on('pinchcancel', defaultAction);
-//	    TOUTH.on('pinchin', defaultAction);
-//	    TOUTH.on('pinchout', defaultAction);
-//	    
-//	    TOUTH.on('rotatestart', defaultAction);
-//	    TOUTH.on('rotatemove', defaultAction);
-//	    TOUTH.on('rotateend', defaultAction);
-//	    TOUTH.on('rotatecancel', defaultAction);
-	    
-//	    TOUTH.on('pinchend', pinchendHandder);
-//	    TOUTH.on('rotateend', rotateendHandder);		
 	}
-	function stepup(){
+	//启动
+	function _stepup(){
 		_buildHTML();
 		_initEvent();
 		if(CONFIG.AUTO_OPEN){
@@ -180,45 +189,86 @@
 	};
 	var ImgWall = function(config){
 		_self=this;
-		//有些手机浏览器有BUG（加载时获得的宽高不正确），给点延时
+		//有些手机浏览器有BUG（加载时获得的宽高不正确），给点延时，防止获取错误
 		window.setTimeout(function(){
 			_initDefault(config);
-			stepup();
-		},50)
+			_stepup();
+		},5)
 	}
-
 	ImgWall.prototype = {
+		//设置过渡时间
+		setTranTime:function(second){
+			ELEMENTS.WALL.style[STYLE.TRANSITION] = 'all ' + second + 's';
+		},
+		//多图模式下轻移
 		slightMove:function(offset){
-			ELEMENTS.WALL.style[STYLE.TRANSFORM] = 'translate(-'+(STYLE.WIN_WIDTH*STATE.CURRENT_INDEX - offset)+'px)';
+			this.setTranTime(0);
+			ELEMENTS.WALL.style[STYLE.TRANSFORM] = 'translate('+(0-(STYLE.WIN_WIDTH*STATE.CURRENT_INDEX + offset))+'px)';
 		},
+		//切换图片
 		toIndex:function(index){
-			STATE.CURRENT_INDEX = index%CONFIG.DATAS.length;
-			ELEMENTS.WALL.style[STYLE.TRANSFORM] = 'translate(-'+STYLE.WIN_WIDTH*STATE.CURRENT_INDEX+'px)';
+			this.setTranTime(CONFIG.TRAN_TIME);
+		    index = index<0?0:index;
+		    index = index>=CONFIG.DATAS.length? CONFIG.DATAS.length-1:index;
+			STATE.CURRENT_INDEX = index;
+			ELEMENTS.IMG = document.getElementById('iw_img_'+STATE.CURRENT_INDEX);
+			ELEMENTS.WALL.style[STYLE.TRANSFORM] = 'translate('+(0-STYLE.WIN_WIDTH*STATE.CURRENT_INDEX)+'px)';
+			var sty = ELEMENTS.IMG.style[STYLE.TRANSFORM];
+			STATE.ZOOM_LAST = /scale\(([\.\d]+)\)/.exec(sty)?RegExp.$1:0;
+			STATE.ROTATE_LAST = /rotate\(([\.\d]+)deg\)/.exec(sty)?RegExp.$1:0;
+			STATE.X_LAST = STATE.X_TMP =  /translateX\((\d+)px\)/.exec(sty)?RegExp.$1:0;
+			STATE.Y_LAST = STATE.Y_TMP =  /translateY\((\d+)px\)/.exec(sty)?RegExp.$1:0;
+			if(EVENTS.SWITCH_EVENT){
+				EVENTS.SWITCH_EVENT.call(this,CONFIG.DATAS[STATE.CURRENT_INDEX]);
+			}
 		},
-		//放大、缩小、旋转
-		resize:function(scale,rotate){
-			
-		},
-		//移动
-		translate:function(x,y){
-			
+		//单图模式：放大、缩小、旋转、轻移
+		transition:function(){
+			ELEMENTS.IMG.style[STYLE.TRANSFORM] ='scale(' + STATE.ZOOM_LAST + ') rotate('+ STATE.ROTATE_LAST +'deg) translateX('+ STATE.X_TMP + 'px) translateY(' + STATE.Y_TMP + 'px)';
 		},
 		//打开
 		open:function(){
-//			alert('open');
+			ELEMENTS.ROOT.style.display = 'block';
+			if(EVENTS.OPEN_EVENT){
+				window.setTimeout(EVENTS.OPEN_EVENT,3);
+			}
 		},
 		//关闭
 		close:function(){
-			
+			ELEMENTS.ROOT.style.display = 'none';
+			if(EVENTS.CLOSE_EVENT){
+				EVENTS.CLOSE_EVENT.call(this);
+			}			
 		},
 		onOpen:function(fn){
-			
+			if(typeof fn == 'function'){
+				EVENTS.OPEN_EVENT = fn;
+			}
 		},
 		onClose:function(fn){
-			
+			if(typeof fn == 'function'){
+				EVENTS.CLOSE_EVENT = fn;
+			}
 		},
 		onSwitch:function(fn){
-			
+			if(typeof fn == 'function'){
+				EVENTS.SWITCH_EVENT = fn;
+			}
+		},
+		//在foot部位添加操作，由于有BUG，继续使用window.setTimeout
+		addAction:function(tip, fn){
+			//有BUG，没办法保证全即时
+			window.setTimeout(function(){
+				var act = document.createElement('div');
+				act.innerHTML = tip;
+				if(ELEMENTS.FOOT.children.length == 0){
+					act.className = 'iw_frist';
+				}
+				act.addEventListener('click',function(evt){
+					fn.call(this,evt,CONFIG.DATAS[STATE.CURRENT_INDEX]);
+				});
+				ELEMENTS.FOOT.appendChild(act);
+			},6);
 		}
 	}
 	if(gobal)window.ImgWall = ImgWall;
